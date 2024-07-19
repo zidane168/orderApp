dbc907e0857660758caa239bdd930c68b7d148b1
Vi Ly <vi.lh@>
2024-07-19 16:53:43 +0700

    max-h-screen p-4 overflow-scroll: moi co the co thanh cuon y

--- src/components/menu/MenuItem.tsx
+++ src/components/menu/MenuItem.tsx
@@ -6,8 +6,9 @@ import toast from 'react-hot-toast'
 import { productApi } from "@/app/api/product/product.api" 
 import { useRouter } from "next/navigation"
 import { IProductExtra, IProductSize } from "@/app/api/product"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { CartContext } from "../AppContext"
+import Image from "next/image"
 interface IMenuItem {
     id: number,
     path: string, 
@@ -15,12 +16,15 @@ interface IMenuItem {
     description: string, 
     basePrice: number, 
     isAddToCart: boolean, 
-    sizes?: IProductSize,
-    extras?: IProductExtra,
+    sizes?: IProductSize[],
+    extras?: IProductExtra[],
 }
 
-export default function MenuItem({ id, path, name, description, basePrice, isAddToCart = true } : IMenuItem) {
+export default function MenuItem({ id, path, name, description, basePrice, isAddToCart = true,
+    sizes, extras 
+} : IMenuItem) {
   
+    const [ showPopup, setShowPopup ] = useState<boolean>(false);
     const router = useRouter();
     async function handleDeleteProduct(ev: React.ChangeEvent<HTMLFormElement>) {
         ev.preventDefault()
@@ -57,8 +61,58 @@ export default function MenuItem({ id, path, name, description, basePrice, isAdd
 
     const { addToCart } = useContext(CartContext)
 
+    function handleAddToCartButtonClick() {
+        if (sizes?.length === 0 && extras?.length === 0) {
+            addToCart(id, 1, sizes, extras);
+            toast.success('Added to cart');
+        } else {
+            setShowPopup(true)
+        }
+    }
+
     return (
         <>
+            {
+                showPopup && (
+                <div className="fixed inset-0 flex items-center justify-center bg-black/80">
+                    <div className="max-w-md max-h-screen p-4 overflow-scroll bg-white rounded-lg"> 
+                        <Image 
+                            src={ path }
+                            alt={ name } 
+                            width={300} height={300}
+                            className="mx-auto"
+                        />
+                        <h2 className="mb-2 text-lg font-bold text-center"> { name }  </h2>
+                        <div className="mb-2 text-sm text-justify leading-[20px] text-gray-500" dangerouslySetInnerHTML={{ __html: description }} />
+                          { sizes?.length > 0 && (
+                            <div className="p-2 bg-gray-300 rounded-md">
+                                <h3> Pick your size </h3>
+                                { sizes?.map (size => (
+                                    <label className="block p-2 py-2 mb-1 border rounded-md"> 
+                                        <input type="radio" name="size"/> { size.name } + <span className="font-semibold text-primary"> ${ size.price } </span>
+                                    </label>
+                                ))}
+                            </div>
+                        )}
+
+                        { extras?.length > 0 && (
+                            <div className="p-2 mt-4 bg-gray-300 rounded-md">
+                                <h3> Pick your Extras </h3>
+                                { extras?.map (extra => (
+                                    <label className="block p-2 py-2 mb-1 border rounded-md"> 
+                                        <input type="checkbox" name="extra"/> { extra.name } +<span className="font-semibold text-primary"> ${ extra.price } </span>
+                                    </label>
+                                ))}
+                            </div>
+                        )}
+
+                        <Button className="mt-4 text-white bg-primary" type="button">
+                            Add to cart "Selected price"
+                        </Button>
+                    </div>
+                </div>
+                )
+            }
             <div className="p-4 text-center transition-all bg-gray-200 rounded-lg hover:bg-white hover:cursor-pointer hover:shadow-2xl">
                 <div className="text-center">
                     <img src={ path } alt="pizza" className="block mx-auto max-h-auto max-h-24" />
@@ -68,7 +122,7 @@ export default function MenuItem({ id, path, name, description, basePrice, isAdd
                 
                 { isAddToCart === true && (
                     <button className="px-6 py-2 mt-2 text-white rounded-full bg-primary"
-                        onClick={ () => addToCart(id) }
+                        onClick={ handleAddToCartButtonClick }
                     > 
                         Add to cart ${ basePrice } 
                     </button>
@@ -95,4 +149,4 @@ export default function MenuItem({ id, path, name, description, basePrice, isAdd
             </div> 
         </>
     )
-}
+}   
